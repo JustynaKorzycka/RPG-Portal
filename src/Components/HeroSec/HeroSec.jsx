9609ae17d4c2b6e5356746f0fc059efec7880066
Justyna Korzycka <justyna.korzycka@gmail.com>
2022-01-19 23:07:02 +0100

    Change hero section

--- src/Components/HeroSec/HeroSec.jsx
+++ src/Components/HeroSec/HeroSec.jsx
@@ -1,11 +1,27 @@
 import './HeroSec.scss'
-import  heroImg  from '../../Assets/img/heroImg.jpg'
+import heroImg from '../../Assets/img/heroImg.jpg'
+import  heroVideo  from '../../Assets/video/milk-way.mp4'
+
 
 const HeroSec = () => {
   return (
     <div className='hero-section'>
-      <h1>Have an adventure in the world of fantasy </h1>
-      <button>Join now</button>
+      <video autoPlay loop muted src={heroVideo} type="video/mp4"></video>
+      <div className="hero-desc">
+        <div className="left-hero">
+          <h1>The word of fantasy is waiting for You!</h1>
+        </div>
+        <div className="right-hero">
+            <p>Imagination is more important than knowledge. For knowledge is limited to all we know and understand, while imagination embraces the entire world, and all there ever will be to know and understand.
+            <span>― Michael Scott, The Warlock </span>
+            </p>
+          <h3>Play on your own rules</h3>
+          <button className='btn-full-width light' >JOIN NOW</button>
+        </div>
+       
+       
+      </div>
+     
     </div>
   )
 }
